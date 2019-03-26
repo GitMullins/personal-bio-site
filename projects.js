3c6038d295836bcc92fc9850c6269abb459ec091
David Mullins <Mullins6323@gmail.com>
2019-03-26 17:37:49 -0500

    buttons fill in info

--- projects.js
+++ projects.js
@@ -20,29 +20,73 @@ let projects = [
 ];
 
 const printToDom = (id, print) => {
-    const selectedId = document.getElementById(id);
-    selectedId.innerHTML = print;
-}
+    document.getElementById(id).innerHTML = print;
+};
 
-const createProjectCards = () => {
+const remove = (divId) => {
+    document.getElementById(divId).remove();
+};
+
+const projectsClick = () => {
     let domString ='';
     for (let i=0; i<projects.length; i++){
         if(projects[i].available == true){
-        domString += '<h4>';
+        domString += '<div id="projectsPage"><h4>';
         domString += `<p>Title: ${projects[i].title}</p>`;
         domString += `<p>Screenshot: ${projects[i].screenshot}</p>`;
         domString += `<p>Description: ${projects[i].description}</p>`;
         domString += `<p>Technologies Used: ${projects[i].technologiesUsed}</p>`;
         domString += `<p>URL: ${projects[i].url}</p>`;
         domString += `<p>GitHubURL: <a href="https://github.com/GitMullins">${projects[i].githubUrl}</a></p>`;
-        domString += '</h4>'
+        domString += '</h4></div>'
     }
     printToDom('projectsPage', domString);
 }
-}
+};
+
+const bioClick = () => {
+    let message = `<div class='card' id='background'>
+    <h3><u><b>My Background:</b></u></h3>
+    <b>Current employer:</b> Tennessee Dept. of Revenue<br>
+    <b>Job Title:</b> Tax Auditor (Nov. 2014 - current)<br>
+    Revenue Officer (Jan. 2013 - October 2013)<br>
+    </div>
+    <div class='card' id='loveDev'>
+    <h3><u><b>What I love about development:</b></u></h3>
+    I love problem solving, and I love that development can be basic or complex depending on the project. The field offers a limitless amount of possibilities to solve a problem, and techniques vary between developers. Development allows me to problem-solve and create solutions that I see the most value in.
+    </div>
+    <div class='card' id='whyDev'>
+    <h3><u><b>Why I want to go into development:</b></u></h3>
+    I enjoy creating efficiency: I recognize the value in making jobs easier, automizing tasks, and gathering/providing useful data. I also have a unique interest in solving problems or creating something that can benefit a lot of people. <br> 
+    </div>`;
+        printToDom('bioFill', message);
+        remove('technologiesPage');
+        remove('projectsPage');
+};
+
+const techClick = () => {
+    let message = `  <div class="fullPage" id="technologiesPage">
+    <h2><b><u>Technologies:</u></b></h2>
+    <ul>
+    <li>GitBash</li>
+    <li>HTML/CSS</li>
+    <li>JavaScript</li>
+    </ul></div>`
+        printToDom('techFill', message);
+        remove('projectsPage');
+        remove('bioFill')
+
+};
+
+
+const navBtns = () => {
+document.getElementById('navToBio').addEventListener('click', bioClick);
+document.getElementById('navToTechnologies').addEventListener('click', techClick);
+document.getElementById('navToProjects').addEventListener('click', projectsClick);
+};
 
 const init = () => {
-    createProjectCards();
-}
+    navBtns();
+};
 
 init();
